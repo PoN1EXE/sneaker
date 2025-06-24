aaa3cde9806ab8442890675d6fdac648884f6140
PoN1EXE <PoN1EXE1@gmail.com>
2025-06-24 19:50:28 +0300

    мелочные правки

--- src/components/ModalBar.tsx
+++ src/components/ModalBar.tsx
@@ -17,22 +17,22 @@ export const ModalBar = ({ isOpen, onClose, cartItems, onRemoveFromCart }: Modal
   const total = cartItems.reduce((sum, item) => sum + parsePrice(item.price), 0)
   const tax = total * 0.05
   return (
-    <Modal isOpen={isOpen} onRequestClose={onClose} ariaHideApp={false}>
+    <Modal isOpen={isOpen} onRequestClose={onClose} ariaHideApp={false} className='drawer'>
       <div className='modal'>
         <h2>Корзина</h2>
 
-        <div className='modal__items'>
+        <div className='drawer__items'>
           {cartItems.map((item) => (
             <CartSneakerPart key={item.cartId} sneaker={item} onRemoveFromCart={() => onRemoveFromCart(item.cartId)} />
           ))}
         </div>
 
-        <div>
-          <p>Итого: {total} руб.</p>
-          <p>Налог 5%: {tax.toFixed(2)} руб.</p>
+        <div className='drawer__footer'>
+          <p>Итого:..........................{total} руб.</p>
+          <p>Налог 5%:.................. {tax.toFixed(2)} руб.</p>
         </div>
 
-        <button className='modal__order-button'>Оформить заказ</button>
+        <button className='drawer__order-button'>Оформить заказ</button>
       </div>
     </Modal>
   )
